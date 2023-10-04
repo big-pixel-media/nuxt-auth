0894999a50590f0262186ca8f148fa4cf62bad1f
Richard Adnams <rick@ironfang.uk>
2023-10-04 21:38:01 +0100

    turned off some eslint rules

--- playground/pages/session.vue
+++ playground/pages/session.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="flex items-center justify-center" v-if="session">
-        <pre>{{ session }}</pre>
+    <div class="flex items-center justify-center">
+        <div v-if="session">
+            <pre>{{ session }}</pre>
+        </div>
+        <div v-if="!session">No session</div>
     </div>
-    <div class="flex items-center justify-center" v-if="!session">No session</div>
 </template>
 <script setup lang="ts">
 import { useAuth } from "#imports";
